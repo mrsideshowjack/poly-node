e8432d42cd71e46bce2d1b0e67ece6cfc0c0caea
Jack Mason <mrsideshowjack@gmail.com>
2017-10-07 15:13:33 +0100

    intial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const public = __dirname + "/public/";
+
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(public + "index.html"));
+})
+
+app.use('/', express.static('public'))
+
+app.listen(8082, function() {
+	console.log("app running on port 8082");
+});
